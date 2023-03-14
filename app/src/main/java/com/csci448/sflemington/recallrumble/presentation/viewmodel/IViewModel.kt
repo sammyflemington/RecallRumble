0caafb80eb25671436ec13ac62c1923a788505fe
lexicooke <acooke@mines.edu>
2023-03-13 22:57:59 -0600

    .

--- app/src/main/java/com/csci448/sflemington/recallrumble/presentation/viewmodel/IViewModel.kt
+++ app/src/main/java/com/csci448/sflemington/recallrumble/presentation/viewmodel/IViewModel.kt
@@ -12,15 +12,10 @@ interface IViewModel {
     val currentGame: QuizPlay?
     val currentQuestionNumber : Int
     val currentQuestionState : Question?
-    //val currentQuestionStatus : QuestionStatus?
+
     val currentViewedUser : User?
-    // Both question index and question count can be changed when Quiz class is implemented
-    // to hold a current quiz state
-    //val currentQuestionIndex: Int
-    //val currentQuizQuestionCount: Int
     fun onUserProfileSaved (name: String, username: String)
     fun newQuizPlay(quizPlay: QuizPlay)
-    //fun updateStatus(correct: Boolean)
     fun setViewedUser(user: User)
 
 }
