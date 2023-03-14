aa93712d952ed90bfef0f1e05c14e9df0db861fd
Sammy Flemington <69018340+sammyflemington@users.noreply.github.com>
2023-03-13 22:54:13 -0600

    quiz creation screen toasts

--- app/src/main/java/com/csci448/sflemington/recallrumble/presentation/navigation/specs/QuizCreationScreenSpec.kt
+++ app/src/main/java/com/csci448/sflemington/recallrumble/presentation/navigation/specs/QuizCreationScreenSpec.kt
@@ -1,10 +1,20 @@
 package com.csci448.sflemington.recallrumble.presentation.navigation.specs
 
 import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
@@ -30,6 +40,20 @@ object QuizCreationScreenSpec : IScreenSpec {
 
     @Composable
     override fun TopAppBarActions(viewModel: IViewModel, navController: NavHostController, navBackStackEntry: NavBackStackEntry?, context: Context){
-
+        val context = LocalContext.current
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "Cancel", modifier = Modifier.clickable { Toast.makeText(context,
+                "Quiz creation will be cancelled",
+                Toast.LENGTH_SHORT
+            ).show() } )
+            Text(text = "Save", modifier = Modifier.clickable {Toast.makeText(context,
+                "Quiz will be saved",
+                Toast.LENGTH_SHORT
+            ).show()})
+        }
     }
 }
