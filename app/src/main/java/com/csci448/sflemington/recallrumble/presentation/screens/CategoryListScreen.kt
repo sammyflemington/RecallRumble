a4e2ac653fa19e1cc2164d3ec3d8e4a4696a61eb
Sammy Flemington <69018340+sammyflemington@users.noreply.github.com>
2023-03-13 20:22:35 -0600

    Clickable categories (doesn't work yet)

--- app/src/main/java/com/csci448/sflemington/recallrumble/presentation/screens/CategoryListScreen.kt
+++ app/src/main/java/com/csci448/sflemington/recallrumble/presentation/screens/CategoryListScreen.kt
@@ -1,6 +1,9 @@
 package com.csci448.sflemington.recallrumble.presentation.screens
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -11,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -23,15 +27,22 @@ import com.csci448.sflemington.recallrumble.data.CategoryRepository
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CategoryListScreen(categories: List<Category>) {
+    val context = LocalContext.current
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
-            modifier = Modifier.width(300.dp)
+            modifier = Modifier.fillMaxWidth()//width(300.dp)
         ) {
             items(categories) {
                 Card(
                     modifier = Modifier
                         .padding(11.dp)
                         .height(122.dp)
+                        .clickable{
+                            Log.d("448.CategoryListScreen", "Category clicked!")
+                            Toast.makeText(context,
+                            "You will be taken into an online match",
+                            Toast.LENGTH_SHORT)
+                        }
                 ) {
                     Column(Modifier.fillMaxWidth(),
                         horizontalAlignment = Alignment.CenterHorizontally)
