819062d9c3a1403dd5a983c6ae761086a2c75762
iceyiso <iceyiso@gmail.com>
2023-03-13 22:51:30 -0600

    Layout

--- app/src/main/java/com/csci448/sflemington/recallrumble/presentation/components/QuizCreationQuestion.kt
+++ app/src/main/java/com/csci448/sflemington/recallrumble/presentation/components/QuizCreationQuestion.kt
@@ -1,8 +1,9 @@
 package com.csci448.sflemington.recallrumble.presentation.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import android.widget.Space
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.*
@@ -13,7 +14,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.csci448.sflemington.recallrumble.data.Question
 import com.csci448.sflemington.recallrumble.data.QuestionRepository
@@ -26,15 +29,30 @@ fun QuizCreationQuestion(question: Question, myNumber : Int){
         mutableStateOf("Answer 3"),
         mutableStateOf("Answer 4"))}
     val correctAnswerIndex = rememberSaveable {mutableStateOf(0)}
+    val notif = rememberSaveable { mutableStateOf("") }
+    if (notif.value.isNotEmpty()) {
+        Toast.makeText(LocalContext.current, notif.value, Toast.LENGTH_LONG).show()
+        notif.value = ""
+    }
 
     Row(){
         //Text("#".plus(myNumber.toString()))
-        Column{
+        Column (Modifier.padding(10.dp)){
             QuizCreationPrompt(prompt.value, myNumber, onEditButtonClicked = {})
             for (i in 0..3){
-                QuizCreationAnswerSlot(answers[i].value, i, correctAnswerIndex.value, onCorrectAnswerClicked = {correctAnswerIndex.value = i})
+                Row(Modifier.padding(10.dp)) {
+                    QuizCreationAnswerSlot(answers[i].value, i, correctAnswerIndex.value, onCorrectAnswerClicked = {correctAnswerIndex.value = i})
+                }
             }
         }
+    }
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = "Cancel", modifier = Modifier.clickable { notif.value = "Cancelled" } )
+        Text(text = "Save", modifier = Modifier.clickable {notif.value = "Saved"})
     }
 }
 
