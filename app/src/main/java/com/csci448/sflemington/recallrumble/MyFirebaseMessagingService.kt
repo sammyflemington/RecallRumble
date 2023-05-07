41bb7b5ac9fd51877b8aedcbbf817abee16d51b6
iceyiso <iceyiso@gmail.com>
2023-05-07 17:35:19 -0600

    Notification Attempt

---
+++ app/src/main/java/com/csci448/sflemington/recallrumble/MyFirebaseMessagingService.kt
@@ -0,0 +1,31 @@
+package com.csci448.sflemington.recallrumble
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import androidx.test.core.app.ApplicationProvider
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    companion object {
+        private const val LOG_TAG = "448.MyFirebaseMessagingService"
+    }
+
+    fun generateNotification(title: String, message: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+    }
+
+
+}
