a0a89dc7a6ff3d6c557e5cb68283b755c4ebb14b
iceyiso <iceyiso@gmail.com>
2023-05-07 18:04:53 -0600

    Notifications

--- app/src/main/java/com/csci448/sflemington/recallrumble/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/csci448/sflemington/recallrumble/MyFirebaseMessagingService.kt
@@ -14,17 +14,57 @@ import androidx.core.content.ContextCompat
 import androidx.test.core.app.ApplicationProvider
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.ktx.remoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     companion object {
         private const val LOG_TAG = "448.MyFirebaseMessagingService"
+
+        private const val channelId = "notification_channel"
+
+        private const val channelName = "com.csci448.sflemington.recallrumble"
     }
 
     fun generateNotification(title: String, message: String) {
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 
+        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT)
+
+        var builder: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.gladiator_win)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000,1000,1000,1000))
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+
+        builder = builder.setContent(getRemoteView(title,message))
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel((notificationChannel))
+        }
+
+        notificationManager.notify(0,builder.build())
+    }
+
+    fun getRemoteView(title: String, message: String): RemoteViews? {
+        val remoteView = RemoteViews("com.csci448.sflemington.recallrumble", R.layout.notification)
+
+        remoteView.setTextViewText(R.id.title,title)
+        remoteView.setTextViewText(R.id.message,message)
+        remoteView.setImageViewResource((R.id.app_logo),R.drawable.gladiator_win)
+
+        return remoteView
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        if(message.getNotification() != null) {
+            generateNotification(message.notification!!.title!!, message.notification!!.body!!)
+        }
     }
 
 
